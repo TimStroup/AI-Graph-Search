0d23b3d1f17c50ffac5b6cb78d800c01452bcf3b
TimStroup <timstroup14@gmail.com>
2019-02-08 17:25:28 -0700

    fixing desktop stuff

--- src/main/java/Driver/AISearchDriver.java
+++ src/main/java/Driver/AISearchDriver.java
@@ -4,8 +4,8 @@ import main.java.Vertices;
 
 public class AISearchDriver {
 
-    public static void main(String[] args){
-        System.out.println(Vertices.V1);
-        System.out.println();
-    }
+	public static void main(String[] args){
+		System.out.println(Vertices.V1);
+		System.out.println();
+	}
 }
